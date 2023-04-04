e3ece1edc86031cf71e0e511c1eb31471a2779af
Fellipe Corominas <corominas.fellipe@hotmail.com>
2023-04-04 10:24:28 -0300

    first commit

---
+++ src/http.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+export { app, httpServer, io };
---
+++ src/websocket.ts
@@ -0,0 +1,87 @@
+
+import { io } from "./http";
+import { v4 as uuidv4 } from "uuid";
+
+type ChatRoom = {
+  id: string;
+  users: string[];
+};
+
+// Define a queue to hold users who are searching for someone to chat with
+const userQueue: string[] = [];
+
+// Define a map to hold chat rooms and their associated users
+const chatRooms: ChatRoom[] = [];
+
+function createChatConnection() {
+  if (userQueue.length >= 2) {
+    const user1 = userQueue.shift()!;
+    const user2 = userQueue.shift()!;
+    const chatRoomId = uuidv4();
+
+    io.to(user1).emit("chatConnected", { chatRoomId, partnerId: user2 });
+    io.to(user2).emit("chatConnected", { chatRoomId, partnerId: user1 });
+
+    // Create a chat room and add the two users to the room
+    const chatRoom: ChatRoom = {
+      id: chatRoomId,
+      users: [user1, user2],
+    };
+    chatRooms.push(chatRoom);
+  }
+}
+
+function endChat(socketId: string) {
+  const chatRoom = chatRooms.find((chatRoom) =>
+    chatRoom.users.includes(socketId)
+  );
+  if (chatRoom) {
+    chatRoom.users.forEach((user) => {
+      io.to(user).emit("chatEnded");
+    });
+    const index = chatRooms.indexOf(chatRoom);
+    chatRooms.splice(index, 1);
+  }
+}
+
+
+io.on("connection", (socket) => {
+
+  socket.on("message", (chatRoomId: string, message: string) => {
+    const chatRoom = chatRooms.find((chatRoom) => chatRoom.id === chatRoomId);
+    if (chatRoom) {
+      chatRoom.users.forEach((user) => {
+        io.to(user).emit("message", {
+          message: message,
+          sender: socket.id,
+          timestamp: Date.now(),
+        });
+      });
+    }
+  });
+
+  socket.on("startSearch", (callback) => {
+    userQueue.push(socket.id);
+    createChatConnection();
+    callback({
+      status: "success"
+    })
+  });
+
+  socket.on("stopSearch", (callback) => {
+    const index = userQueue.indexOf(socket.id);
+    userQueue.splice(index, 1);
+    callback({
+      status: "success"
+    })
+  });
+
+  socket.on("endChat", () => {
+    endChat(socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    endChat(socket.id);
+  });
+
+});
