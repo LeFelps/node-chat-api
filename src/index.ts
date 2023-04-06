be6b516a1a6fcad41348bb6358a253f7512dc6cf
Fellipe Corominas <corominas.fellipe@hotmail.com>
2023-04-05 21:49:48 -0300

    Port and host changed

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { httpServer } from "./http";
 import "./websocket";
 
-const port = 10000
+const port = 3333
 
-httpServer.listen(10000, () =>
+httpServer.listen({
+  host: "0.0.0.0",
+  port: port,
+}, () =>
   console.log(`Server is running on port ${port}`)
 );
